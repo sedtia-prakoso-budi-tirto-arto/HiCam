ea65f79640e0ac87c10f482b48b728c5573d8148
WhatShitTooYaa <hendriarkan@gmail.com>
2023-12-05 15:28:47 +0700

    sudah bisa kamera dan kordinat

--- app/src/main/java/com/example/fp/MainActivity.java
+++ app/src/main/java/com/example/fp/MainActivity.java
@@ -5,32 +5,237 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 1;
+    private static final int PERMISSION_REQUEST_STORAGE = 2;
+    private static final int PERMISSION_REQUEST_LOCATION = 3;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+//    imageView imageViewGetLocation;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         View kameraView = findViewById(R.id.kameraView);
-        kameraView.setOnClickListener(new View.OnClickListener() {
+        kameraView.setOnClickListener(v -> openKameraView());
+
+        View camView = findViewById(R.id.camView);
+        camView.setOnClickListener(v -> openKameraView());
+
+        View imageViewGetLocation = findViewById(R.id.Peta);
+
+        imageViewGetLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openKameraView();
+                requestLocationUpdates();
             }
         });
 
-        View camView = findViewById(R.id.camView);
-        camView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openKameraView();
-            }
-        });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        }
+
+        // Memeriksa dan meminta izin penyimpanan eksternal
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        } else {
+            // Izin sudah diberikan, inisialisasi LocationManager
+            initializeLocationManager();
+        }
     }
+
 
     private void openKameraView() {
         Intent kamera = new Intent(this, KameraView.class);
         startActivity(kamera);
+    }
+
+
+    private void initializeLocationManager() {
+        // Check if the app has the location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            // Check if GPS or network location providers are enabled
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+                // Start listening for location updates
+                startLocationUpdates();
+            } else {
+                // Show a message to the user to enable GPS or network
+                Toast.makeText(this, "Enable GPS or network location", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // Request location permission from the user
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+
+    // Mulai mendengarkan pembaruan lokasi
+    private void startLocationUpdates() {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Mendapatkan longitude dan latitude
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+
+                // Menampilkan Toast dengan informasi lokasi
+                Toast.makeText(MainActivity.this, "Longitude: " + longitude + "\nLatitude: " + latitude, Toast.LENGTH_SHORT).show();
+
+                // Hentikan pembaruan lokasi setelah mendapatkan satu pembaruan
+                locationManager.removeUpdates(this);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1000,
+                1,
+                locationListener);
+
+    }
+
+    private void requestLocationUpdates() {
+        // Check location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            // Initialize location manager
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            // Check if GPS or network location providers are enabled
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+                // Initialize location listener
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        // Display latitude and longitude in a Toast
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        Toast.makeText(MainActivity.this, "Latitude: " + latitude + "\nLongitude: " + longitude, Toast.LENGTH_LONG).show();
+
+                        // Stop location updates after receiving the first location
+                        locationManager.removeUpdates(this);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                    }
+                };
+
+                // Request location updates
+                locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+
+            } else {
+                // Show a message to the user to enable GPS or network
+                Toast.makeText(this, "Enable GPS or network location", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // Request location permission from the user
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, request location updates
+                requestLocationUpdates();
+            } else {
+                // Permission denied, show a message to the user
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
